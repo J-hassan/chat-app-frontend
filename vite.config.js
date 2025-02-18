708a2586d4f32b8b2e20145c83bb11b07eaba5da
J-hassan <imuhammadhassanktk@gmail.com>
2025-02-18 19:45:07 +0500

    changes to vite.config.js file

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    proxy: {
+      "/api": {
+        // This is the path that will be proxied
+        target: "https://chat-app-backend-three-xi.vercel.app", // Your backend server URL
+        changeOrigin: true, // Required for CORS to work
+        rewrite: (path) => path.replace(/^\/api/, ""), // Optional: Remove the /api prefix
+      },
+    },
+  },
+});
