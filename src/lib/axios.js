262e706c066af06fdd601ef6ac137d3f8db41c0a
J-hassan <imuhammadhassanktk@gmail.com>
2025-02-18 21:35:43 +0500

    some axios configuration of baseURL

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,9 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL:
-    import.meta.env.MODE === "development"
-      ? "http://localhost:5001/api"
-      : "https://chat-app-backend-three-xi.vercel.app/api",
+  baseURL: import.meta.env.VITE_API_URL,
   withCredentials: true,
 });
